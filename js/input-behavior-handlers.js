071fc983d9c18c844d1ba6ec86c5d7f61494fd65
Juan Bernardo <jrivera@hugeinc.com>
2016-12-04 12:26:28 -0500

    removing right handler

--- js/input-behavior-handlers.js
+++ js/input-behavior-handlers.js
@@ -15,6 +15,6 @@ export function blurHandler (e) {
   if (!/date__|datepicker__/.test(e.target.className)) {
     const datepickerShown = document.querySelector('.datepicker--show');
     datepickerShown && datepickerShown.classList.remove('datepicker--show');
+    document.removeEventListener('click', blurHandler);
   }
-  document.removeEventListener('click', inputHandler);
 }
