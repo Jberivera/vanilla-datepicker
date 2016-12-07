71dbe8890755d147151e50548092450746d10f15
Juan Bernardo <jrivera@hugeinc.com>
2016-12-06 21:53:27 -0500

    Tab handler

--- js/input-handler.js
+++ js/input-handler.js
@@ -1,3 +1,5 @@
+const TAB = 9;
+
 export default function inputHandler (e) {
   const datepickerShown = document.querySelector('.datepicker--show');
 
@@ -5,6 +7,7 @@ export default function inputHandler (e) {
     datepickerShown.classList.remove('datepicker--show');
   } else {
     document.addEventListener('click', blurHandler);
+    document.addEventListener('keyup', tabHandler);
   }
   setTimeout(() => {
     e.target.parentNode.classList.add('datepicker--show');
@@ -16,5 +19,15 @@ function blurHandler (e) {
     const datepickerShown = document.querySelector('.datepicker--show');
     datepickerShown && datepickerShown.classList.remove('datepicker--show');
     document.removeEventListener('click', blurHandler);
+    document.removeEventListener('keyup', tabHandler);
+  }
+}
+
+function tabHandler (e) {
+  if (e.keyCode === TAB && !/date__|datepicker__/.test(e.target.className)) {
+    const datepickerShown = document.querySelector('.datepicker--show');
+    datepickerShown && datepickerShown.classList.remove('datepicker--show');
+    document.removeEventListener('click', blurHandler);
+    document.removeEventListener('keyup', tabHandler);
   }
 }
